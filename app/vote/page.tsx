c3f47a899dd5f5f43f04348b0c418f0b5d11998a
Michael R. Krisnadhi <michaelkrisnadhi@gmail.com>
2024-09-26 22:27:49 +0700

    update source

--- app/vote/page.tsx
+++ app/vote/page.tsx
@@ -1,5 +1,10 @@
 import AuthMiddleware from "@/utils/middleware/AuthMiddleware";
 import VotePage from "./components/VotePage";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Voter Dashboard"
+};
 
 export default function Page() {
   return (
